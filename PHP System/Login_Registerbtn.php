8caebe8d067210188e7859e4c435bf9a05cf817e
BrownieSen <161237905+BrownieSen@users.noreply.github.com>
2025-11-03 11:26:14 +0800

    Delete PHP System directory

--- PHP System/Login_Registerbtn.php
+++
@@ -1,3 +0,0 @@
-<?php
-header("Location: Register.html");
-exit();
--- PHP System/Logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION["user"]);
-unset($_SESSION["logged_in"]);
-header("Location: Login.html");
-exit();
